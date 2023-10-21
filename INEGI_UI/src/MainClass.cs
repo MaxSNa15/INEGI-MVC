a3a197560095bcbc2a86ad7477dc4cf9cb62192e
MaxSNava <max.s015@outlook.com>
2023-10-20 22:25:18 -0600

    first commit

---
+++ INEGI_UI/src/MainClass.cs
@@ -0,0 +1,11 @@
+namespace INEGI.src
+{
+    static class MainClass
+    {   
+        [STAThread]
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ INEGI_UI/src/databases/INEGI.sql
@@ -0,0 +1,57 @@
+CREATE DATABASE CensoPoblacion;
+
+USE CensoPoblacion;
+
+-- Tabla Vivienda
+CREATE TABLE Vivienda (
+    ID_Vivienda INT AUTO_INCREMENT PRIMARY KEY,
+    Direccion VARCHAR(255) NOT NULL,
+    Tipo ENUM('concreto', 'adobe antiguo', 'ladrillo', 'madera', 'cartón', 'piedra', 'prefabricada', 'material ecológico', 'paja/ramas/caña', 'adobe moderno') NOT NULL,
+    FOREIGN KEY (ID_Municipio) REFERENCES Localidades(ID_Localidad)
+);
+
+-- Tabla Municipios
+CREATE TABLE Municipios (
+    ID_Municipio INT AUTO_INCREMENT PRIMARY KEY,
+    Nombre VARCHAR(255) NOT NULL
+);
+
+-- Tabla Localidades
+CREATE TABLE Localidades (
+    ID_Localidad INT AUTO_INCREMENT PRIMARY KEY,
+    Nombre VARCHAR(255) NOT NULL,
+    ID_Municipio INT,
+    FOREIGN KEY (ID_Municipio) REFERENCES Municipios(ID_Municipio)
+);
+
+-- Tabla Habitante
+CREATE TABLE Habitante (
+    ID_Habitante INT AUTO_INCREMENT PRIMARY KEY,
+    Nombre VARCHAR(255) NOT NULL,
+    FechaNacimiento DATE NOT NULL,
+    Genero ENUM('Masculino', 'Femenino', 'Otro') NOT NULL,
+    ID_Vivienda INT,
+    FOREIGN KEY (ID_Vivienda) REFERENCES Vivienda(ID_Vivienda)
+);
+
+-- Tabla ActividadEconomica
+CREATE TABLE ActividadEconomica (
+    ID_Actividad INT AUTO_INCREMENT PRIMARY KEY,
+    Descripcion VARCHAR(255) NOT NULL
+);
+
+-- Tabla ViviendaActividad (relación muchos a muchos entre Vivienda y ActividadEconomica)
+CREATE TABLE ViviendaActividad (
+    ID_Vivienda INT,
+    ID_Actividad INT,
+    PRIMARY KEY (ID_Vivienda, ID_Actividad),
+    FOREIGN KEY (ID_Vivienda) REFERENCES Vivienda(ID_Vivienda),
+    FOREIGN KEY (ID_Actividad) REFERENCES ActividadEconomica(ID_Actividad)
+);
+
+-- Tabla Usuario
+CREATE TABLE Usuario (
+    ID_Usuario INT AUTO_INCREMENT PRIMARY KEY,
+    Nickname VARCHAR(255) NOT NULL,
+    Contrasena VARCHAR(255) NOT NULL
+);
